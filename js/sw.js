b272b92100d6c3e6c0f946a650b58c27daffcc14
GihanGat <6135528499+GihanGat@users.noreply.github.com>
2019-11-25 17:13:07 -0500

    version 4.0.0
    Use the techniques we described in class to create a service worker that will allow your ToDo application to work offline.
    1) Create the script to load the ServiceWorker
    2) Load the Workbox library
    3) Create a Precache directive
    4) Create three route directives
       i) one for HTML
       ii) one for CSS
       iii) one for JavaScript

---
+++ js/sw.js
@@ -0,0 +1,32 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute([
+    '/index.html',
+    '/js/*.js',
+    '/js/sw.js',
+    '/img/add.svg',
+    '/img/edit.svg',
+    '/img/remove.svg'
+]);
+
+workbox.routing.registerRoute(
+    /\.js$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: "todoApp-js-cache",
+    })
+);
+
+workbox.routing.registerRoute(
+    /\.htm(l?)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: "todoApp-html-cache",
+    })
+);
+
+workbox.routing.registerRoute(
+    /\.css$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: "todoApp-css-cache",
+    })
+);
+
